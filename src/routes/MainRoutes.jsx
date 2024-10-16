37440191dcef28d2675704ec41b31205cdb510a2
drsantos93 <dustin.santos93@gmail.com>
2024-10-16 09:54:40 +0800

    lol

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,14 +1,20 @@
 import MainLayout from "../components/layouts/MainLayout"
 import Dashboard from "../pages/Dashboard"
-const MainRoutes = {
-    path: '/',
-    element: <MainLayout/>,
-    children:[
-        {
-            path: "",
-            element: <Dashboard/>
-        },
-    ]
-}
+const MainRoutes = [
+    {
+        path: '/',
+        element: <MainLayout/>,
+        children:[
+            {
+                path: "",
+                element: <Dashboard/>
+            },
+            {
+                path: "/profiles",
+                element: <Profiles/>
+            }
+        ]
+    }
+]
 
 export default MainRoutes
